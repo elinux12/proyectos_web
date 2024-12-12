a0eab9556ac8cfe9c23293fdb5615c984e88cbb0
elinux12 <michael_perez_1@hotmail.com>
2024-12-11 20:52:30 -0600

    se realizo el ejercicio 6

---
+++ js/ejercicios/jscript/06-numerosPositivos.js
@@ -0,0 +1,18 @@
+/**
+Crear algoritmo que regrese la cantidad
+ * de numeros positivos de un array
+*/
+
+let array=[2,5,7,15,-5-100,55];
+
+function cuantosPositivos(arr){
+  let contador =0;
+  for (i in arr){
+    if (arr[i]>=0){
+        contador+=1;
+    }
+  }
+  return contador;
+}
+let resultado = cuantosPositivos(array);
+console.log(resultado);
