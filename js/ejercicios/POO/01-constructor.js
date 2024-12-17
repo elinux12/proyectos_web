ca75e199905b15bc2ee61549a9c9776b2ffa80be
elinux12 <michael_perez_1@hotmail.com>
2024-12-17 16:05:19 -0600

    Se realizo el ejercicio 01 de POO

--- js/ejercicios/POO/01-constructor.js
+++ js/ejercicios/POO/01-constructor.js
@@ -1,1 +1,19 @@
-console.log("HOla");
+/**
+Usar el metodo MARTH.RANDOM para generar un numero random 
+ * asignarle ese random a una propiedad id en Usuario
+
+*/
+
+function Usuario(name){
+    this.name= name;
+    this.id= Math.random();
+}
+
+let user = new Usuario('Felipe');
+let user2 = new Usuario('Chanchito');
+console.log(user,user2);
+
+
+/**
+Usuario {name: 'Felipe', id: num random}
+*/
